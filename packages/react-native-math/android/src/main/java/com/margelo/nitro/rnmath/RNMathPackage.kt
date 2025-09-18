eeed829cd87c27d05c15eb9100b56199337dc3bb
tuannc2 <tuannc2@vng.com.vn>
2025-09-18 16:28:28 +0700

    implement for android

--- packages/react-native-math/android/src/main/java/com/margelo/nitro/rnmath/RNMathPackage.kt
+++ packages/react-native-math/android/src/main/java/com/margelo/nitro/rnmath/RNMathPackage.kt
@@ -16,7 +16,7 @@ class RNMathPackage : TurboReactPackage() {
 
     companion object {
         init {
-            System.loadLibrary("rnmath")
+            System.loadLibrary("RNMath")
         }
     }
 }
